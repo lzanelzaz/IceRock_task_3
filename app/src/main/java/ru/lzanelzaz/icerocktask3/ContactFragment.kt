7df3d5d618a299e6a199117c0cb4866c9f679d18
lzanelzaz <lzanelzaz@gmail.com>
2022-06-21 14:41:50 +0700

    Change contact UI
    Change from ConstraintLayout to LinearLayout(horizontal)
    
    Add MaterialCardView

--- app/src/main/java/ru/lzanelzaz/icerocktask3/ContactFragment.kt
+++ app/src/main/java/ru/lzanelzaz/icerocktask3/ContactFragment.kt
@@ -23,10 +23,6 @@ class ContactFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
--- app/src/main/java/ru/lzanelzaz/icerocktask3/ContactsFragment.kt
+++ app/src/main/java/ru/lzanelzaz/icerocktask3/ContactsFragment.kt
@@ -23,10 +23,6 @@ class ContactsFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
